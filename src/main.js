0a6882c7304cad7dcc4ff6650ab50e91ae557e0f
mahzza <mahsa.79mirmiran@gmail.com>
2020-04-30 12:13:44 +0430

    edited 1
    add router and maked routes.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
+Vue.use(VueRouter);
 
+const router = new VueRouter({
+  routes : routes,
+  mode:'history'
+});
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router :router
 }).$mount('#app')
