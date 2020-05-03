355fcadd6f61f701691e1bcbc96e2f01a03379b6
mahzza <mahsa.79mirmiran@gmail.com>
2020-05-03 11:45:20 +0430

    added a logo image

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,13 +2,15 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container">
-            <a class="navbar-brand" href="#">Logo</a>
+        <img class="logo" src="@/assets/fruit.png" alt="Image alt" />
+
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto w-100 justify-content-end">
+                    <li><router-link class="nav-link" to="/" exact>mivefurushieMa</router-link></li>
                    <li><router-link class="nav-link" to="/apple" exact>Apple<span class="sr-only">(current)</span></router-link></li>
                     <li><router-link class="nav-link" to="/kiwi" exact>kiwi</router-link></li>
                     <li><router-link class="nav-link" to="/banana" exact>banana</router-link></li>
@@ -27,5 +29,7 @@ export default {
 </script>
 
 <style>
-
+.logo{
+    width: 6%;
+}
 </style>
